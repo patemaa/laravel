d25fad3b12fd431251307180c80ee0d83adf7ef0
Fatma R <anthoschichequs@gmail.com>
2025-01-17 14:25:54 +0300

    nav icin ayri bir dosya olusturuldu.

--- resources/views/Components/layout.blade.php
+++ resources/views/Components/layout.blade.php
@@ -7,10 +7,11 @@
 </head>
 <body>
 <nav>
-    <a href="/">Home</a>
-    <a href="/about">About</a>
-    <a href="/contact">Contact</a>
+    <x-nav-link href="/"> Home</x-nav-link>
+    <x-nav-link href="/about">About</x-nav-link>
+    <x-nav-link href="/contact">Contact</x-nav-link>
 </nav>
+
 {{--    <?php echo $slot ?>--}}
 {{$slot}}
 </body>
