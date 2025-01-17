f7f3177be883d5c429cfe6d594be34e94b7a1168
Fatma R <anthoschichequs@gmail.com>
2025-01-17 13:55:07 +0300

    about ve contact sayfalari icin about.php ve contact.php olusturuldu, gerekli route'lar eklendi.

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,14 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+//Route::get('/about', function () {
+//    return 'About page';
+//});
+
+Route::get('/about', function () {
+    return view('about');
+});
+
+Route::get('/contact', function () {
+    return view('contact');
+});
